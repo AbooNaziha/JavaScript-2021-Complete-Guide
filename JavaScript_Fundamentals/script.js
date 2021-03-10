be5e5a18215125771706d1e9535d4a68fd51be2f
Aboo Naziha <shaibu.ism2@gmail.com>
2021-03-10 15:13:21 +0100

    Fundamental_part_1_JavaScript

---
+++ JavaScript_Fundamentals/script.js
@@ -0,0 +1,28 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+
+let firstName = "Matida";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let Jonas_Matida = 'JM'
+let $function = 27;
+
+let person = 'Jonas'
+let PI = 3.142
+console.log(PI)
+
+let myFirstJob = 'Programmer'
+let myCurrentJob = 'Teacher'
+
+let job1 = 'Programmer'
+let job2 = 'Teacher'
+
+console.log(myFirstJob)
+
