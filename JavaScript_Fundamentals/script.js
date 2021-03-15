c146f2709d7ceeeb6f2a7e617a38ce11782b302b
Aboo Naziha <shaibu.ism2@gmail.com>
2021-03-15 08:16:11 +0100

    part_one_progress_file

--- JavaScript_Fundamentals/script.js
+++ JavaScript_Fundamentals/script.js
@@ -1,9 +1,11 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
 console.log('Jonas');
 console.log(23);
 
+//Varaible name conventions
 
 let firstName = "Matida";
 console.log(firstName);
@@ -25,4 +27,135 @@ let job1 = 'Programmer'
 let job2 = 'Teacher'
 
 console.log(myFirstJob)
+
+
+//data types 
+
+true;
+console.log(true);
+
+let JavaScriptIsFun = true;
+console.log(JavaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof JavaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+JavaScriptIsFun = 'YES'
+console.log(typeof JavaScriptIsFun)
+
+let year;
+console.log(year);
+console.log(typeof year)
+
+year = 1991;
+console.log(year)
+console.log(typeof year)
+
+console.log(typeof null)
+
+
+// let const and var
+
+let age = 30;
+age = 31
+
+const birthYear = 1991
+// birthYear = 1990 
+
+// const job;
+
+var job = 'Programmer'
+job = 'Teacher'
+
+let lastName = 'Ismail'
+console.log(lastName)
+
+
+// Basic Operators 
+
+// maths operations
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah =  now - 2020
+console.log(ageJonas, ageSarah)
+
+console.log(ageJonas*2, ageJonas/10, 2**3)
+// 2**3 means 2 to the power of 3 = 2*2*2
+
+const firstName = 'Jonas'
+const lastName = 'Ismail'
+console.log(firstName + ' ' + lastName);
+
+// assignment operations
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--
+console.log(x);
+
+// comparison operations 
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+
+
+// Operator precedance 
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah =  now - 2018;
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25-10-5
+
+console.log(x, y)
+
+const average = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, average)
+*/
+
+// coding challenge 1
+
+// TEST DATA ONE
+let MarkWeight = 78
+let MarkHeight = 1.69 
+
+let JohnWeight = 92
+let JohnHeight = 1.95
+
+MarkBMI = MarkWeight / (MarkHeight**2)
+console.log(MarkBMI)
+
+JohnBMI = JohnWeight / (JohnHeight**2)
+console.log(JohnBMI)
+
+markHigherBMI = MarkBMI > JohnBMI
+console.log(markHigherBMI)
+
+// TEST DATA TWO
+
+let MarkWeight2 = 95
+let MarkHeight2 = 1.88
+
+let JohnWeight2 = 85
+let JohnHeight2 = 1.76
+
+MarkBMI2 = MarkWeight2 / (MarkHeight2**2)
+console.log(MarkBMI2)
+
+JohnBMI2 = JohnWeight2 / (JohnHeight2**2)
+console.log(JohnBMI2)
+
+markHigherBMI2 = MarkBMI2 < JohnBMI2
+console.log(markHigherBMI2)
+
+
+
 
