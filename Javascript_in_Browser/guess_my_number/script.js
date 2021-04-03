c4a95a6a82c928f458ede1240e884bc5e8576876
Aboo Naziha <shaibu.ism2@gmail.com>
2021-04-03 14:15:58 +0100

    guess_my_number

---
+++ Javascript_in_Browser/guess_my_number/script.js
@@ -0,0 +1,96 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+// DOM manipulation allows JavaScript to interact with the web passage
+
+// DOM: Document Object Model is a structured representation of HTML Documents. Allows Javascript to access HTML elements and styles to manipulate them.
+
+// Basically a dom is a connection point betwwen HTML documents and JavaScript codes
+// Note: the dom methods and properties are part of the web API
+document.querySelector('.message').textContent = '🎉Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 15;
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  // When there is no input
+  if (!guess) {
+    displayMessage('⛔ No Number!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉Correct Number!');
+
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When guess is too high
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('🔥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+// else if (guess > secretNumber) {
+//     if (score > 1) {
+//       document.querySelector('.message').textContent = ' 📈 Too high!';
+//       score--;
+//       document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.message').textContent = '🔥 You lost the game!';
+//       document.querySelector('.score').textContent = 0;
+//     }
+
+//     // When guess is too low
+//   } else if (guess < secretNumber) {
+//     if (score > 1) {
+//       document.querySelector('.message').textContent = '📉 Too low!';
+//       score--;
+//       document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.message').textContent = '🔥 You lost the game!';
+//       document.querySelector('.score').textContent = 0;
+//     }
+//   }
+// });
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  document.querySelector('.number').style.width = '15rem';
+});
