e47323edaf7b7ecaaec86cc58475aa932d9acac7
Aboo Naziha <shaibu.ism2@gmail.com>
2021-08-20 08:03:30 +0100

    continuation_how_javascript_works_behind_screen

--- BehindTheSenes/script.js
+++ BehindTheSenes/script.js
@@ -70,7 +70,7 @@ let y = 2;
 const z = 3;
 console.log(x === window.x);
 
-*/
+
 
 console.log(this);
 
@@ -81,7 +81,7 @@ const calAge = function (birthYear) {
 
 calAge(1991);
 
-// Arrow function this keyword poinrs to the global scope
+// Arrow function this keyword points to the global scope
 
 const calAgeArrow = birthYear => {
   console.log(2037 - birthYear);
@@ -89,3 +89,138 @@ const calAgeArrow = birthYear => {
 };
 
 calAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+
+jonas.calAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calAge = jonas.calAge;
+//The above method is called method borrowing
+
+matilda.calAge();
+
+const f = jonas.calAge;
+f();
+
+
+
+// Regular function and arrow function
+//var firstName = 'Matilda';
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+
+    // solution onw
+    // const self = this; //self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    //   //console.log(this.year >= 1981 && this.year <= 1996);
+    //   //inside a regular function call the this function is undefined.
+    // };
+
+    //solution two
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+      //console.log(this.year >= 1981 && this.year <= 1996);
+      //inside a regular function call the this function is undefined.
+    };
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+
+jonas.greet();
+jonas.calAge();
+//Never use an error method as a function
+
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5, 6, 8);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  a + b;
+};
+
+addArrow(2, 5, 6);
+
+*/
+
+//Premitive versus object reference
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+
+const friend = me;
+friend.age = 27;
+console.log('friend:', friend);
+console.log('me', me);
+
+//Primitive types
+let lastName = 'Williams';
+let oldlastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldlastName);
+
+//Reference types
+const jessica = {
+  firstName: 'Jessics',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedjessica = jessica;
+marriedjessica.lastName = 'davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage:', marriedjessica);
+
+//Copying objects
+const jessica2 = {
+  firstName: 'Jessics',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
+//object.assign is a shallow copy.
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
+
+//Learn how to create a deep clone
